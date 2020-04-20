88fc08c15040865db4e2f6be5ca318c6d6b6f454
leemamas <leemamas@admin.com>
2020-04-20 20:23:56 +0800

    form组件添加~

--- app/views.py
+++ app/views.py
@@ -1,25 +1,48 @@
 from django.shortcuts import render, redirect, HttpResponse
 from .models import *
+from django import forms
+from django.forms import widgets
+
+class BookForm(forms.Form):
+    title = forms.CharField(max_length=32,label='书名')
+    price = forms.DecimalField(max_digits=8, decimal_places=2,label='价格')
+    date = forms.DateField(
+        label = '日期',
+        widget=widgets.TextInput(attrs={'type':'date'})
+    )
+    publish=forms.ModelChoiceField(
+        label='出版社',
+        queryset=Publish.objects.all(),
+        empty_label=None
+    )
+    authors=forms.ModelMultipleChoiceField(
+        label='作者',
+        queryset=Author.objects.all()
+    )
 
 def book(request):
     list = Book.objects.all()
     return render(request, 'book.html', locals())
 
+
 def add_book(request):
     if request.method == 'POST':
-        title = request.POST.get('title')
-        price = request.POST.get('price')
-        date = request.POST.get('date')
-        publish = request.POST.get('publish')
-        authors = request.POST.getlist('authors')
+        form = BookForm(request.POST)
 
-        book_obj = Book.objects.create(title=title, price=price, date=date, publish_id=publish)
-        book_obj.authors.add(*authors)
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            price = form.cleaned_data.get('price')
+            date = form.cleaned_data.get('date')
+            publish = form.cleaned_data.get('publish')
+            authors = form.cleaned_data.get('authors')
 
-        return redirect('/book/')
+            book = Book.objects.create(title=title, price=price, date=date, publish=publish)
+            book.authors.add(*authors)
 
-    publishs = Publish.objects.all()
-    authors = Author.objects.all()
+            return redirect('/book/')
+
+    form = BookForm()
+
     return render(request, 'book_add.html', locals())
 
 
